3661a53aaf1f7ac1c1e577cd1d9c5dde027b54e3
Crystal Lopez <crystallopez289@gmail.com>
2020-04-29 23:56:10 +0000

    added functionality for quitting and addition

---
+++ calculator.py
@@ -0,0 +1,15 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+while True: 
+    equation = input(">")
+    acceptable_operands = ["+","-","*","/","square","cube","pow","mod"]
+    tokenized_equation = equation.split(" ")
+    if tokenized_equation[0] == "q":
+        quit()
+    else:
+        if tokenized_equation[0] == "+":
+            print(add(float(tokenized_equation[1]),float(tokenized_equation[2])))
+            break
+            
